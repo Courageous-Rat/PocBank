16e278aac99e0524d81153e8440025ffd2ca4e72
Patrick Braunstorfer <patrick.braunstorfer@outlook.com>
2018-05-17 12:52:10 +0200

    Implement basic log spamming to local Elastic Search via Serilog

--- src/SerilogToKibana/SerilogToKibana/Program.cs
+++ src/SerilogToKibana/SerilogToKibana/Program.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SerilogToKibana
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var configuration = CreateSerilogConfiguration();
+            var logger = configuration.CreateLogger();
+            var random = new Random();
 
+            while(true)
+            {
+                var waitTime = random.Next(1000) * 30;
+                Thread.Sleep(waitTime);
+                logger.Information(Guid.NewGuid().ToString());
+            }
+        }
+
+        private static LoggerConfiguration CreateSerilogConfiguration()
+        {
+            var elasticSearchOptions = new ElasticsearchSinkOptions(new Uri("localhost:9200"))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                
+            };
+
+            return new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch();
         }
     }
 }
