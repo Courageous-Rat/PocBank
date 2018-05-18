a5878ae7a4bcbbc5e8526944732071c8fa9b6a72
Patrick Braunstorfer <patrick.braunstorfer@outlook.com>
2018-05-18 14:46:02 +0200

    Add custom fields to NLog target configuration

--- src/NlogToKibana/NlogToKibana/Program.cs
+++ src/NlogToKibana/NlogToKibana/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NLog;
+using NLog.Config;
+using System;
+using System.Threading;
 
 namespace NlogToKibana
 {
@@ -6,7 +9,15 @@ namespace NlogToKibana
     {
         static void Main(string[] args)
         {
-            
+            var logger = LogManager.GetLogger("Kibana");
+            var random = new Random();
+
+            while (true)
+            {
+                var waitTime = random.Next(1000);
+                Thread.Sleep(waitTime);
+                logger.Info(Guid.NewGuid().ToString());
+            }
         }
     }
 }
